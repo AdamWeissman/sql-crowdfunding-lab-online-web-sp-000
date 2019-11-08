2dafc5f56ff1460749a6fe45d4406ba940248a98
Adam Weissman <adam@comedytragedyepic.com>
2019-11-08 10:07:43 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE projects (
 id INTEGER PRIMARY KEY,
-title TEXT, 
-category TEXT, 
+title TEXT,
+category TEXT,
 funding_goal NUMERIC,
 start_date TEXT,
 end_date TEXT
@@ -9,8 +9,8 @@ end_date TEXT
 ;
 
 CREATE TABLE users (
-id INTEGER PRIMARY KEY
-name TEXT
+id INTEGER PRIMARY KEY,
+name TEXT,
 age INTEGER
 )
 ;
